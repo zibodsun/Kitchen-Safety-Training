3164b14a068c6627620ec4945e90bec9af551b61
FSE_VR23 <FSE_VR23@LAPTOP-OE4HKTPU>
2024-04-15 11:47:03 +0100

    Implemented Scene Transition Object

---
+++ Assets/_Zibo/Scripts/SceneTransManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransManager : MonoBehaviour
+{
+    public AnimationClip fadeAnimation;
+    public Animator anim;
+    public bool debugLoadScene;
+
+    float _fadeTime;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        _fadeTime = fadeAnimation.length;
+    }
+
+    private void Update()
+    {
+        if (debugLoadScene) { 
+            LoadNextScene(); 
+            debugLoadScene = false;
+        }
+    }
+
+    public void LoadNextScene() {
+        anim.Play("FadeOut");
+        StartCoroutine(LoadSceneDelayed(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadSceneDelayed(int buildIndex) {
+        yield return new WaitForSeconds(_fadeTime);
+        SceneManager.LoadScene(buildIndex);
+        anim.Play("FadeIn");
+    }
+}
